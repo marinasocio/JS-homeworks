aff4b0a9cd46f251deae7b893086c77ad0373913
Marina Kuliieva <marinasocio@gmail.com>
2019-07-03 14:37:34 +0300

    ex.1,2

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-alert('Hello world!');
-console.log(2019);
+// Excercise 1
+
+function multiply() {
+  let a = 1;
+  if (arguments.length == 0) return 0;
+  for (let i = 0; i < arguments.length; i++) a *= arguments[i];
+  return a;
+}
+console.log('Excercise 1: ', multiply(1, 2, 3));
+
+// Excercise 2
+
+function reverseString(str) {
+  let l = 1;
+  let result = '';
+  for (let i = 1; i < str.length+1; i++) {
+    result = result.concat(str[str.length-l]);
+    l += 1;
+  }
+  return result;
+}
+console.log('Excercise 2: ', reverseString('Marina'));
+
+
+
+
