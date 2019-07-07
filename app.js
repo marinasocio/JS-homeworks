16e1d8d539fde10660bc3230ede3570a95af5c21
Marina Kuliieva <marinasocio@gmail.com>
2019-07-07 15:23:14 +0300

    functions exs.1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Excercise 1
+/*// Excercise 1
 
 function multiply() {
   let a = 1;
@@ -18,7 +18,7 @@ function multiply() {
 
 function reverseString(str) {
   let l = 1;
-  let result = '';
+  let result = "";
 
   for (let i = 1; i < str.length + 1; i++) {
     result = result.concat(str[str.length - l]);
@@ -31,10 +31,10 @@ function reverseString(str) {
 // Excercise 3
 
 function charSet(str) {
-  let result = '';
+  let result = "";
 
   for (let i = 0; i < str.length; i++) {
-    result = result.concat(' ', str.charCodeAt(i));
+    result = result.concat(" ", str.charCodeAt(i));
   }
 
   return result;
@@ -46,16 +46,14 @@ guessNumber();
 
 function guessNumber(b) {
   while (true) {
-    let a = Math.floor(Math.random() * (9)) + 1;
+    let a = Math.floor(Math.random() * 9) + 1;
     b = prompt("Введите число от 1 до 10");
 
     if (b < 0 || b > 10) {
       alert("Вы ввели число не из диапазона 0 - 10");
-
     } else if (b == a) {
       alert("Вы выиграли!");
       break;
-
     } else {
       alert("Вы не угадали ваше число " + b + " а выпало число " + a);
       break;
@@ -72,7 +70,6 @@ function getArray() {
   let N = prompt("Введите любое целое число > 1 для создания масива:");
 
   if (N > 0 && N % 1 === 0) {
-
     for (var i = 1; i <= N; i++) {
       foo.push(i);
     }
@@ -85,27 +82,99 @@ function getArray() {
 
 // Excercise 6
 
-doubleArray([1, 2, 3])
+doubleArray([1, 2, 3]);
 
 function doubleArray(arr) {
   return arr.concat(arr);
 }
 
-
 // Excercise 7
 
 let arrs = [];
 arrs[0] = [1, 2, 3];
-arrs[1] = ['b', 'c'];
-arrs[2] = ['marina', 'albina'];
+arrs[1] = ["b", "c"];
+arrs[2] = ["marina", "albina"];
 
 let arrRows = [];
 
-arrs = arrs.map(function (item) {
+arrs = arrs.map(function(item) {
   return item.splice(1, arrs.length);
 });
 
 arrRows = arrRows.concat.apply(arrRows, arrs);
+
+// Excercise 8
+
+const users = [
+  {
+    name: "Marina",
+    age: 32,
+    gender: "female"
+  },
+  {
+    name: "Denis",
+    age: 30,
+    gender: "male"
+  },
+  {
+    name: "Olga",
+    age: 25,
+    gender: "female"
+  }
+];
+*/
+// Функции высшего порядка
+
+// Exsercise 1
+
+function firstFunc(arr, secondFunc) {
+  let res = [];
+  
+	for (let i = 0; i < arr.length; i++) {
+		res.push(secondFunc(arr[i]));
+	}
+
+	return res;
+}
+
+function handler1(el) {
+  if (!el) return el;
+  
+	return el[0].toUpperCase() + el.slice(1);
+}
+
+function handler2(el) {
+	if (!el) {
+		return el;
+	} else {
+		return (el *= 10);
+	}
+}
+
+function handler3(el) {
+	return (el = el.name + ' is ' + el.age);
+}
+
+function handler4(el) {
+	return el.split('').reverse().join('');
+}
+
+let question = firstFunc(['my', 'name', 'is', 'Trinity'], handler1);
+question = question.join('');
+
+let numbers = firstFunc([10, 20, 30], handler2);
+numbers = numbers.join(' ');
+
+let users = firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'} ], handler3);
+users = users.join(', ');
+
+let reverse = firstFunc(['abc', '123'], handler4);
+reverse = reverse.join(', ');
+
+// console.log('New value: ', question);
+// console.log('New value: ', numbers);
+// console.log('New value: ', users);
+// console.log('New value: ', reverse);
 
 
 
